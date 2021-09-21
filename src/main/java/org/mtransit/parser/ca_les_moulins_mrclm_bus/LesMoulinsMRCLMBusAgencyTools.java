326136db6c358c04be4161084e1b877409573fee
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-21 08:43:11 -0400

    Cleanup

--- src/main/java/org/mtransit/parser/ca_les_moulins_mrclm_bus/LesMoulinsMRCLMBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_les_moulins_mrclm_bus/LesMoulinsMRCLMBusAgencyTools.java
@@ -1,20 +1,21 @@
 package org.mtransit.parser.ca_les_moulins_mrclm_bus;
 
+import static org.mtransit.commons.RegexUtils.DIGITS;
+import static org.mtransit.commons.StringUtils.EMPTY;
+
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CleanUtils;
 import org.mtransit.commons.RegexUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.mt.data.MAgency;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static org.mtransit.commons.StringUtils.EMPTY;
 
 // https://exo.quebec/en/about/open-data
 // https://exo.quebec/xdata/mrclm/google_transit.zip
@@ -30,6 +31,12 @@ public class LesMoulinsMRCLMBusAgencyTools extends DefaultAgencyTools {
 		return "exo Terrebonne-Mascouche (Les Moulins)";
 	}
 
+	@Nullable
+	@Override
+	public List<Locale> getSupportedLanguages() {
+		return LANG_FR;
+	}
+
 	@Override
 	public boolean defaultExcludeEnabled() {
 		return true;
@@ -41,6 +48,11 @@ public class LesMoulinsMRCLMBusAgencyTools extends DefaultAgencyTools {
 		return MAgency.ROUTE_TYPE_BUS;
 	}
 
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
+	}
+
 	@NotNull
 	@Override
 	public String cleanRouteLongName(@NotNull String routeLongName) {
@@ -49,68 +61,19 @@ public class LesMoulinsMRCLMBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabel(routeLongName);
 	}
 
-	private static final String B = "B";
-	private static final String C = "C";
-	private static final String G = "G";
-	private static final String T = "T";
-
-	private static final String MT = "MT";
-
-	private static final long RID_ENDS_WITH_B = 2_000L;
-	private static final long RID_ENDS_WITH_C = 3_000L;
-	private static final long RID_ENDS_WITH_G = 7_000L;
-
-	private static final long RID_STARTS_WITH_T = 20_000L;
-
-	private static final long RID_STARTS_WITH_MT = 1_320_000L;
-
-	private static final String RSN_EXPH = "EXPH";
-	private static final String RSN_EXPM = "EXPM";
-	private static final String RSN_EXPR = "EXPR";
-
-	private static final long RID_EXPH = 99_001L;
-	private static final long RID_EXPM = 99_002L;
-	private static final long RID_EXPR = 99_003L;
-
 	@Override
-	public long getRouteId(@NotNull GRoute gRoute) {
-		//noinspection deprecation
-		if (!Utils.isDigitsOnly(gRoute.getRouteId())) {
-			if (RSN_EXPH.equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return RID_EXPH;
-			} else if (RSN_EXPM.equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return RID_EXPM;
-			} else if (RSN_EXPR.equalsIgnoreCase(gRoute.getRouteShortName())) {
-				return RID_EXPR;
-			}
-			Matcher matcher = DIGITS.matcher(gRoute.getRouteShortName());
-			if (matcher.find()) {
-				int digits = Integer.parseInt(matcher.group());
-				if (gRoute.getRouteShortName().startsWith(MT)) {
-					return RID_STARTS_WITH_MT + digits;
-				}
-				if (gRoute.getRouteShortName().startsWith(T)) {
-					return RID_STARTS_WITH_T + digits;
-				}
-				if (gRoute.getRouteShortName().endsWith(B)) {
-					return RID_ENDS_WITH_B + digits;
-				} else if (gRoute.getRouteShortName().endsWith(C)) {
-					return RID_ENDS_WITH_C + digits;
-				} else if (gRoute.getRouteShortName().endsWith(G)) {
-					return RID_ENDS_WITH_G + digits;
-				}
-			}
-			throw new MTLog.Fatal("Unexpected route ID for %s!", gRoute);
-		}
-		return super.getRouteId(gRoute);
+	public boolean defaultRouteIdEnabled() {
+		return true;
 	}
 
-	private static final String AGENCY_COLOR = "1F1F1F"; // DARK GRAY (from GTFS)
-
-	@NotNull
 	@Override
-	public String getAgencyColor() {
-		return AGENCY_COLOR;
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@Override
+	public boolean defaultAgencyColorEnabled() {
+		return true;
 	}
 
 	@NotNull
@@ -185,8 +148,6 @@ public class LesMoulinsMRCLMBusAgencyTools extends DefaultAgencyTools {
 		}
 		return super.getStopCode(gStop);
 	}
-
-	private static final Pattern DIGITS = Pattern.compile("[\\d]+");
 
 	@Override
 	public int getStopId(@NotNull GStop gStop) {
